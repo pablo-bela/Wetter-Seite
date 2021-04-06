3ef5a8ee39f27020adedcdfc3911e831d8ed1568
RoyalMiner <81974635+RoyalMiner@users.noreply.github.com>
2021-04-06 13:53:24 +0200

    Update index.php

--- index.php
+++ index.php
@@ -12,12 +12,12 @@
   
   <body>
     <body background="hintergrund.jpg">
-<style color= white >
+
   <center>
      <h1>Wetter</h1>
      <h2>Wetter für Langenpreising</h2>
   </center>
- </style>
+ 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
   </body>
