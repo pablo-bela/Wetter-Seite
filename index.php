8bb0d480d2d45d7949250c391e0493b148982daf
TmNiklas <44237957+TmNiklas@users.noreply.github.com>
2021-04-06 15:46:26 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,56 +1,58 @@
 <!doctype html>
 <html lang="en">
-  <head>
+<head>
     <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+    <meta name="description" content="">
+    <meta name="keywords" content="Wetter, Langenpreising Wetter, Wartenberg Wetter">
+    <meta name="author" content="RoyalMiner">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Nunito&display=swap" rel="stylesheet">
     <title>website</title>
-  </head>
-  
+</head>
 
-    <body style= "font-family: 'Nunito', sans-serif;" background="hintergrund.jpg">
+<body style= "font-family: 'Nunito', sans-serif;" background="hintergrund.jpg">
 
-      
-  <center>
-     <h1 style="color:White">Wetter</h1>
-        <br>
-     <h2 style="color:White">Wetter für Wartenberg</h2>
-        <br>
-        <br>
-  </center>
-      
-      
-      
-<div class= "container">
-  <div class= "row">
-    <div class= "col-lg-4 col-sm-6"> </div>
-    <div class= "col-lg-4 col-sm-6">
-    <div class="card align-middle shadow-lg rounded" style="width: 18rem;">
-     <img src="https://www.vg-wartenberg.de/wp-content/uploads/2018/07/wartenberg.jpg" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      </div>
-     <ul class="list-group list-group-flush">
-    <li class="list-group-item">An item</li>
-    <li class="list-group-item">A second item</li>
-    <li class="list-group-item">A third item</li>
-     </ul>
-      <div class="card-body">
-       <a href="#" class="card-link">Card link</a>
-         <a href="#" class="card-link">Another link</a>
+<center>
+    <h1 style="color:White">Wetter</h1>
+    <br>
+    <h2 style="color:White">Wetter für Wartenberg</h2>
+    <br>
+    <br>
+</center>
+<section class="slice slice-lg">
+    <div class= "container">
+        <div class= "row row-grid justify-content-around align-items-center">
+            <div class= "col-lg-4 col-sm-6"> </div>
+            <div class= "col-lg-4 col-sm-6">
+                <div class="card shadow-lg rounded hover-translate-y-n10" style="width: 18rem;">
+                    <img src="https://www.vg-wartenberg.de/wp-content/uploads/2018/07/wartenberg.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">An item</li>
+                        <li class="list-group-item">A second item</li>
+                        <li class="list-group-item">A third item</li>
+                    </ul>
+                    <div class="card-body">
+                        <a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>
+                    </div>
+                </div>
+                <div class= "col-lg-4 col-sm-6"> </div>
+            </div>
         </div>
-        </div>
-        <div class= "col-lg-4 col-sm-6"> </div>    
-        </div>
-        </div>
-        </div>
-      
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
-  </body>
+    </div>
+</section>
+
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
+</body>
 </html>
