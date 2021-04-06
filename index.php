b9973144447a1396aeec5414347cd01fa9d9ae10
RoyalMiner <81974635+RoyalMiner@users.noreply.github.com>
2021-04-06 13:50:23 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 
@@ -13,17 +12,14 @@
   <style color= white >
 
   </style>
-  
-  <span style="color:white"> Weiße Schrift </span>
   <body>
-
     <body background="hintergrund.jpg">
-      
-<center>
-  <h1>Wetter</h1>
-  <h2>Wetter für Langenpreising</h2>
-</center>
-
+<style="background-color:white>
+  <center>
+     <h1>Wetter</h1>
+     <h2>Wetter für Langenpreising</h2>
+  </center>
+ </style>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
   </body>
