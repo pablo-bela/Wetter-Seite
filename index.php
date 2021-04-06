7148c2d55e2c680fd198e55cd80fc93b590ffaa7
RoyalMiner <81974635+RoyalMiner@users.noreply.github.com>
2021-04-06 15:15:57 +0200

    Update index.php

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
       
 <center> 
     <div class="card" style="width: 18rem;">
-     <img src="..." class="card-img-top" alt="...">
+     <img src="https://www.vg-wartenberg.de/wp-content/uploads/2018/07/wartenberg.jpg"
       <div class="card-body">
         <h5 class="card-title">Card title</h5>
         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
