48ed461c04edcea120aa85c5be669d3d7cb54c78
RoyalMiner <81974635+RoyalMiner@users.noreply.github.com>
2021-04-07 16:08:57 +0200

    Update index.php

--- index.php
+++ index.php
@@ -48,7 +48,7 @@ $Stadt = $Wetter->name;
   <div class="card-body">
     <h4 class="card-title">Danke dass du diese Seite verwendest</h4>
     <p font-size="13px"; class="card-text">Diese Seite wurde von TmNiklas und Royal_Miner erstellt.</p>
-    <a href="https://github.com/RoyalMiner/Wetter-Seite"; target:"blank"; class="btn btn-primary">Github</a>
+    <a href="https://github.com/RoyalMiner/Wetter-Seite"; target="_blank"; class="btn btn-primary">Github</a>
   </div>
 </div>
             
