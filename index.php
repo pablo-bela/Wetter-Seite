787ffdf5fc812311b80654a1578a8a58f9937e8b
RoyalMiner <81974635+RoyalMiner@users.noreply.github.com>
2021-04-07 15:53:22 +0200

    Update index.php

--- index.php
+++ index.php
@@ -46,7 +46,7 @@ $Stadt = $Wetter->name;
   <div class="card-body">
     <h5 class="card-title">Special title treatment</h5>
     <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="http://github.com/royalminer" target:_blank class="btn btn-primary">Github</a>
+    <a href="http://github.com/royalminer" target:"_blank" class="btn btn-primary">Github</a>
   </div>
 </div>
   
